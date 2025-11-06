34ad97d6625630a4fa500ae85ea6e08619fd9742
Sushil Sharma <58214222+thesushilsharma@users.noreply.github.com>
2025-11-07 02:11:34 +0400

    add rate limiter

--- src/lib/RateLimiter/limiter.ts
+++ src/lib/RateLimiter/limiter.ts
@@ -3,3 +3,5 @@ import { RateLimiter } from "limiter";
 // Rate limiter to prevent brute force attacks
 export const loginLimiter = new RateLimiter({ tokensPerInterval: 5, interval: 'minute' });
 export const signupLimiter = new RateLimiter({ tokensPerInterval: 3, interval: 'hour' });
+export const forgotPasswordLimiter = new RateLimiter({ tokensPerInterval: 3, interval: 'hour' });
+export const resendVerificationLimiter = new RateLimiter({ tokensPerInterval: 5, interval: 'hour' });
